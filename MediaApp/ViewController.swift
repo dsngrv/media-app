6d64219a61f5c107f230be3f04d6022d090cb606
Дмитрий Снигирев <dmitry@iMac-Dmitrij.local>
2023-12-06 02:41:52 +0400

    first commit

---
+++ MediaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediaApp
+//
+//  Created by Дмитрий Снигирев on 06.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
